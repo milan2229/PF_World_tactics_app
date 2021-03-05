e050964e4a97d045dc45773152f60d208a2df1f5
milan2229 <milan_1899.22@icloud.com>
2021-03-05 14:19:20 +0000

    second commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4bde8abd014b90b3fb50512e7912bb8f0c8c491cd27ee116d1433097b565eefa119e9d659afed4e7a5ec2326ab0134a130785e3458b37580db23d39c8cbb1716'
+
