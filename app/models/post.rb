f1ce2551dc8bcb022ea26cce5f21b65cc421512e
milan2229 <milan_1899.22@icloud.com>
2021-03-07 14:06:07 +0000

    search finish

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ApplicationRecord
   # attachment :image
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
   validates :title, presence:true
   validates :body, presence:true
-  
+
 end
