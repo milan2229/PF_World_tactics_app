2fbc24be7e6727c8d8d313d21fedfaa1b68983aa
milan2229 <milan_1899.22@icloud.com>
2021-03-15 02:11:44 +0000

    fix finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     posts_path
   end
-  
-  
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
 class PostsController < ApplicationController
-# before_action :authenticate_user!,except: [:index]
+
   def new
     @post = Post.new
   end
 
   def index
-    # @posts = Post.page(params[:page]).reverse_order
     @search = Post.ransack(params[:q])
     @posts = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(10)
-    # @search_produc= @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(5) # productsの検索結果一覧
     @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(8).pluck(:post_id))
   end
 
