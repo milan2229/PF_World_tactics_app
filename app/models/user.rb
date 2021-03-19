b8fce056d4b6ee0c06e48bef8beca13191505438
milan2229 <milan_1899.22@icloud.com>
2021-03-19 12:57:13 +0000

    fixed layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :following_user, through: :follower, source: :followed
   has_many :follower_user, through: :followed, source: :follower
+  has_many :following_user, through: :follower, source: :followed
   # foreign_keyは入口  sourceは出口
 
   # ユーザーをフォローする
