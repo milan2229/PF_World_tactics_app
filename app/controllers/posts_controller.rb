cd684c8c35e3d84319aed47bfe7e003c489884e2
milan2229 <milan_1899.22@icloud.com>
2021-03-30 08:45:45 +0000

    fixed update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to post_path(post)
+    redirect_to post_path(@post)
   end
 
   def destroy
