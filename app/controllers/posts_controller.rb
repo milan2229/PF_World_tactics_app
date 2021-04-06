82246228be99a7bfa980766a0fdd61e4e373f178
milan2229 <milan_1899.22@icloud.com>
2021-04-06 13:51:38 +0000

    fixed tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,11 @@ class PostsController < ApplicationController
   end
 
   def index
-    # @post = Post.find(params[:id])
+    # @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
     @search = Post.ransack(params[:q])
-    @posts = @search.result(distinct: true).order(created_at: "DESC").
-      includes(:user).page(params[:page]).per(10)
+    # @search = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
+    @posts = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(10)
+    @posts = @posts.joins(:tag_relationships).where(tag_relationships: {tag_id: params[:tag_id]}) if params[:tag_id].present?
     @all_ranks = Post.find(Favorite.group(:post_id).
     order(Arel.sql('count(post_id) desc')).limit(8).pluck(:post_id))
   end
@@ -19,9 +20,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    # @post = Post.new(post_params)
     @post = current_user.posts.build(post_params)
-    tag_list = params[:post][:tag_ids].split(',')
+    tag_list = params[:post][:tag_ids].split('、')
     @tags = tag_list
     @post.user_id = current_user.id
     if @post.save
@@ -36,17 +36,24 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    @tag_list = @post.tags.pluck(:name).join(",")
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
+    tag_list = params[:post][:tag_ids].split(',')
+    if @post.update_attributes(post_params)
+      @post.save_tags(tag_list)
+      flash[:notice] = '投稿を編集しました‼'
+      redirect_to @post
+    else
+     flash[:alert] = "内容が正しくありません。"
+     render 'edit'
+    end
   end
 
   def destroy
     @post = Post.find(params[:id])
-    # binding.pry
     @post.destroy
     redirect_to posts_path
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -14,9 +14,17 @@ class Post < ApplicationRecord
 
 
   def save_tags(savepost_tags)
-      savepost_tags.each do |new_name|
-      post_tag = Tag.find_or_create_by(name: new_name)
-      self.tags << post_tag
+      current_tags = self.tags.pluck(:name) unless  self.tags.nil?
+      old_tags = current_tags - savepost_tags
+      new_tags = savepost_tags - current_tags
+
+      old_tags.each do |old_name|
+        self.tags.delete Tag.find_by(name: old_name)
+      end
+
+      new_tags.each do |new_name|
+        post_tag = Tag.find_or_create_by(name: new_name)
+        self.tags << post_tag
    end
   end
 
