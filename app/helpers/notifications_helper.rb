9ba9f9d99ba29b02e0737ed00c0ea6170ffee41d
milan2229 <milan_1899.22@icloud.com>
2021-04-15 13:56:23 +0000

    finish

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -7,7 +7,7 @@ module NotificationsHelper
     @visiter_comment = notification.post_comment_id
 
     case  notification.action
-    when "following" then
+    when "follow" then
       tag.a(notification.visiter.name, href:users_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
     when  "favorite" then
       tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にいいねしました"
